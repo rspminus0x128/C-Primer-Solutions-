3f8dab9360c3b6cf44db4b36477faba6aa229949
Siddharth292004 <sidharth9509@gmail.com>
2025-07-28 12:37:11 +0530

    intial commit

--- basics/program_01.cpp
+++ basics/program_01.cpp
@@ -1,13 +1,13 @@
 // Write a program in C++ to print welcome text on a separate line.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     cout << "\n\nPrint welcome text on a separate line.\n";
     cout << "--------------------------------------\n";
     cout << "Welcome to,\n";
     cout << "The World of C++\n";
-
 }
