947c9492d584fbc9e8abdb972357ae03de4bc165
Siddharth292004 <sidharth9509@gmail.com>
2025-07-29 20:53:34 +0530

    initial commit

---
+++ patterns/pattern_03.cpp
@@ -0,0 +1,25 @@
+//  *****
+// ****
+// ***
+// **
+// *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5 + 1 - i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
