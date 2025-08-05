313e3947415d36ab27bbf2846526882009ccf4e3
Siddharth292004 <sidharth9509@gmail.com>
2025-08-05 22:31:43 +0530

    initial commit

---
+++ patterns/pattern_53.cpp
@@ -0,0 +1,38 @@
+//  #####
+ //  ****
+ //   ###
+ //    **
+ //     #
+
+ #include <iostream>
+ using namespace std;
+
+ int main()
+ {
+    int i,j;
+    int n = 5;
+
+    for(i =n; i>=1; i--)
+    {
+      for(j = n; j >=1; j--)
+      {
+         if ( i >=j)
+         {
+            if ( i % 2 == 0)
+            {
+               cout << "*";
+            }
+            else{
+               cout << "#";
+            }
+         }
+         else{
+            cout << " ";
+         }
+      }
+      cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+ }
