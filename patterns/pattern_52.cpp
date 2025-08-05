7661d8ab6235a9ed310bf7fb52d831186e0ce6d4
Siddharth292004 <sidharth9509@gmail.com>
2025-08-05 07:31:47 +0530

    initial commit

---
+++ patterns/pattern_52.cpp
@@ -0,0 +1,41 @@
+//     1
+//    *2
+//   3*3
+//  *4*4
+// 5*5*5
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    int n = 5;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n; j >= 1; j--)
+        {
+
+            if (i >= j)
+            {
+                if (j % 2 == 0)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << i;
+                }
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
