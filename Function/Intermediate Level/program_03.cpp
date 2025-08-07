8ff79ed06eeddc6fe18004081e71a0c4152431e7
Siddharth292004 <sidharth9509@gmail.com>
2025-08-07 22:33:06 +0530

    initial commit

---
+++ Function/Intermediate Level/program_03.cpp
@@ -0,0 +1,29 @@
+//  Function to find the largest of three numbers
+
+#include <iostream>
+using namespace std;
+
+int FindLargest(int a, int b, int c);
+int main()
+{
+    int a, b, c;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    cout << "Enter the thrid number: ";
+    cin >> c;
+
+    cout << "\nThe largest number is " << FindLargest(a, b, c);
+    return 0;
+}
+
+int FindLargest(int a, int b, int c)
+{
+    if (a >= b && a <= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
